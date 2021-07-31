7744d33103f449ebae853989d7d55e54561e265d
jkampich1411 <jakob.kampichler@gmail.com>
2021-07-31 21:45:47 +0200

    setup mysql conn to server

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
+/**
+ * @author Jakob Kampichler <jakobkampichler+dev@gmail.coms>
+ * @file This script uses some APIs to get more information about a Minecraft user. It is developed for future-events, but if you know how to modify it, you can use it for yourself, that is why .
+ * @copyright JakobKampichler Dev (2021) - FutureEvents
+ * other meta:
+ * @version 0.1.0
+*/
+
 const fs = require('fs');
 const https = require('https');
 const nodefetch = require('node-fetch');
+const sqlcon = require('mysql');
+var debug = 2;
+
+// MySQL conn (insert into Whitelist)
+function inpSQLWL(uuid, uname) {
+    let conn = sqlcon.createConnection({
+        host: 'itm-hof.tk',
+        user: 'jkampichler_db3',
+        password: 'Future@1227',
+        database: 'jkampichler_db3'
+    });
+    conn.connect();
+    conn.query(`INSERT INTO \`mysqlwl_players\` (\`UUID\`, \`user\`) VALUES (\'${uuid}\', \'${uname}\');`, (err, ress, fields) => {
+        if (err) throw err;
+    });
+    conn.end();
+}
+
+// MySQL conn (remove from Whitelist)
+function delSQLWL(uuid, uname) {
+    let conn = sqlcon.createConnection({
+        host: 'itm-hof.tk',
+        user: 'jkampichler_db3',
+        password: 'Future@1227',
+        database: 'jkampichler_db3'
+    });
+    conn.connect();
+    conn.query(`DELETE FROM \`mysqlwl_players\` WHERE \`UUID\` = \'${uuid}\'`, (err, ress, fields) => {
+        if (err) throw err;
+    });
+    conn.end();
+}
 
 // Get the UUID of the User to do other stuff with it.
 function fetchUUID(name) {
@@ -41,13 +81,13 @@ function fetchSkin(id, playername) {
         let buff = await res.buffer();
 
         fs.writeFile(`${plname}.png`, buff, () => {
-            console.log('dl finished, test');
+            console.log('dl finished');
         });
     }
     dlskin(skurl, playername)
 }
 
-// Prepare UUID to use in MyWl 
+// Prepare UUID to use in MyWl
 function prepUUID(str, usrname) {
     let uid = str;
     let localdata = [8, 12, 16, 20, "-"];
@@ -66,9 +106,12 @@ function prepUUID(str, usrname) {
 
 function cb(nodash, dash, usr) {
     fetchSkin(nodash, usr)
-    console.log(nodash)
-    console.log(dash)
-    console.log(usr)
+    if (debug === 0) return;
+    if (debug === 1) inpSQLWL(dash, usr);
+    if (debug === 2) delSQLWL(dash, usr);
+    console.log(nodash);
+    console.log(dash);
+    console.log(usr);
 }
 
-fetchUUID("thejakobcraft")
+return fetchUUID("thejakobcraft")

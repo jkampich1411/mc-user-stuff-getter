3c2645d78965d2e580a87ad29b4691cda5964183
Jakob Kampichler <jkampich1411@users.noreply.github.com>
2021-08-11 21:51:30 +0200

    almost done

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 /**
  * @author Jakob Kampichler <jakobkampichler+dev@gmail.com>
- * @file This script uses some APIs to get more information about a Minecraft user. It is developed for future-events, but if you know how to modify it, you can use it for yourself, that is why .
+ * @file This script uses some APIs to get more information about a Minecraft user. It is developed for future-events, but if you know how to modify it, you can use it for yourself.
  * @copyright JakobKampichler Dev (2021) - FutureEvents
  * other meta:
  * @version 0.1.0
 */
+
+const moduleDescription = "===[ MC User Stuff Getter ]===\nThank you for using \'MC User Stuff Getter\'!\nGitHub Repository: \'jkampich1411/mc-user-stuff-getter\'\n===[ MC User Stuff Getter ]===\n";
+const advancedModuleDescription = "===[ MC User Stuff Getter ]===\nThank you for using \'MC User Stuff Getter\'!\n(Yes I couldn't think of a better name.)\nGitHub Repository: \'jkampich1411/mc-user-stuff-getter\'\n===[ MC User Stuff Getter ]===\n\n(c) 2021 - theJakobcraft\n";
+
+// CustomLogger
+function deblog(text, lvl) {
+    if(!(lvl)) lvl===1;
+    if(lvl ===1) {
+        console.log(`INFO: ${text}`);
+    }
+    else if(lvl ===2) {
+        console.log(`WARN: ${text}`);
+    }
+    else if(lvl ===3) {
+        console.log(`ERROR: ${text}`);
+    }
+    else if(lvl ===4) {
+        console.log(`CRITICAL: ${text}`);
+    }
+}
 
 const fs = require('fs');
 const https = require('https');
@@ -13,44 +33,86 @@ const sqlcon = require('mysql');
 const exp = require('express');
 const web = exp();
 const app = require('http').createServer(web)
+const hdb = require('handlebars');
+const e = require('express');
+const path = require('path');
 
-var port = 3000;
-var debug = 1;
+var skin;
+var sknTempl;
 
-// Webserver to display Skin
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`)
-});
-web.use(exp.static(__dirname + '/skintemp'));
+require('dotenv').config()
 
-// MySQL conn (insert into Whitelist)
-function inpSQLWL(uuid, uname) {
-    let conn = sqlcon.createConnection({
-        host: 'itm-hof.tk',
-        user: 'jkampichler_db3',
-        password: 'Future@1227',
-        database: 'jkampichler_db3'
-    });
-    conn.connect();
-    conn.query(`INSERT INTO \`mysqlwl_players\` (\`UUID\`, \`user\`) VALUES (\'${uuid}\', \'${uname}\');`, (err, ress, fields) => {
-        if (err) throw err;
-    });
-    conn.end();
+// Check if critical files exist!
+function checkFiles() {
+    
+    try {
+        if (!(fs.existsSync('./skintemp'))) fs.mkdirSync('./skintemp');
+        if (!(fs.existsSync('./skintemp/html'))) fs.mkdirSync('./skintemp/html');
+        if (!(fs.existsSync('./templ'))) fs.mkdirSync('./templ');
+    } catch (e) { throw e; }
+
+    try {
+        if (!(fs.existsSync('./templ/.env'))) {
+            let dl = fs.createWriteStream("./templ/.env");
+            let req = https.get("https://raw.githubusercontent.com/jkampich1411/mc-user-stuff-getter/templ/env", async (res) => {
+                res.pipe(dl);
+
+                res.on('end', () => {
+                    copyEnv();
+                })
+            });
+        };
+
+        if (!(fs.existsSync("./templ/showskin.html"))) {
+            let dl = fs.createWriteStream("./templ/showskin.html")
+            let req = https.get("https://raw.githubusercontent.com/jkampich1411/mc-user-stuff-getter/templ/showskin.html", async (res) => {
+                res.pipe(dl);
+
+                res.on('end', () => {
+                    createSkinTemplate();
+                });
+            });
+        }
+    } catch (e) { throw e; }
+}
+function copyEnv() {
+    try {
+        if(!fs.existsSync('./.env')) {
+            let envTempl = fs.readFileSync('templ/.env', 'utf-8');
+            fs.writeFileSync('./.env', envTempl, err => {
+                if (err) throw err;
+            });
+        }
+    } catch (e) { throw e; }
 }
 
-// MySQL conn (remove from Whitelist)
-function delSQLWL(uuid, uname) {
-    let conn = sqlcon.createConnection({
-        host: 'itm-hof.tk',
-        user: 'jkampichler_db3',
-        password: 'Future@1227',
-        database: 'jkampichler_db3'
-    });
-    conn.connect();
-    conn.query(`DELETE FROM \`mysqlwl_players\` WHERE \`UUID\` = \'${uuid}\'`, (err, ress, fields) => {
-        if (err) throw err;
-    });
-    conn.end();
+function createSkinTemplate() {
+    sknTempl = fs.readFileSync('templ/showskin.html', 'utf-8');
+    skin = hdb.compile(sknTempl);
+}
+
+checkFiles()
+
+var port = process.env.PORT || 8080;
+
+// Use this script in other scripts!
+module.exports = {
+    fetch: function(mcPN) {
+        fetchUUID(mcPN);
+    },
+    startServer: function() {
+        // Webserver to display Skin
+        app.listen(port, () => {
+            console.log(`Server running on port: ${port}`)
+        });
+        web.use('utils/skin', exp.static(__dirname + '/skintemp/html'));        
+    },
+    moduleInfo: function() {
+        return deblog(moduleDescription, 1)
+    },
+    secretModuleInfo: function(){
+        return deblog(advancedModuleDescription, 1)
+    }
 }
 
 // Get the UUID of the User to do other stuff with it.
@@ -82,18 +144,16 @@ function fetchUUID(name) {
     req.end();
 }
 
-// Get the Users Skin
-function fetchSkin(id, playername) {
-    let skurl = `https://crafthead.net/skin/${id}`;
-    async function dlskin(url, plname) {
-        let res = await nodefetch(url);
-        let buff = await res.buffer();
+// Get a users skin in a HTML file
+function fetchSkin(playername) {
+    let data = skin({uname: playername});
+    let outputFileString = `skintemp/html/${playername}.html`;
 
-        fs.writeFile(`skintemp/${plname}.png`, buff, () => {
-            console.log('dl finished');
-        });
-    }
-    dlskin(skurl, playername)
+    fs.writeFile(outputFileString, data, err => {
+        if (err) throw err;
+
+        console.log(`File saved to \'${outputFileString}\'`);
+    });
 }
 
 // Prepare UUID to use in MyWl
@@ -114,15 +174,12 @@ function prepUUID(str, usrname) {
 
 
 function cb(nodash, dash, usr) {
-    fetchSkin(nodash, usr)
-/*
-    if (debug === 0) return;
-    if (debug === 1) inpSQLWL(dash, usr);
-    if (debug === 2) delSQLWL(dash, usr);
-*/
+    fetchSkin(usr)
     console.log(nodash);
     console.log(dash);
     console.log(usr);
 }
 
-return fetchUUID("thejakobcraft2")
+process.on('SIGTERM', () => {
+    app.close();
+});
